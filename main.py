91e7975983ee41e8de5354de482c83fbc5f47036
DimitrijKDM <127964621+DimitrijKDM@users.noreply.github.com>
2023-04-01 15:38:27 +0200

    Gleichungen aufgestellt um unbekannte (n) und (u) herauszufinden

--- main.py
+++ main.py
@@ -19,28 +19,28 @@ class RichtungsvektorErsteGleichung:
         self.richtungsvektor_x = richtungsvektor_x
 
 
-class StuetzvektorErsteGleichung_2_Geraden:
+class StuetzvektorErsteGleichung2Geraden:
     def __init__(self, stuetzvektor_x, stuetzvektor_y, stuetzvektor_z):
         self.stuetzvektor_z = stuetzvektor_z
         self.stuetzvektor_y = stuetzvektor_y
         self.stuetzvektor_x = stuetzvektor_x
 
 
-class RichtungsvektorErsteGleichung_2_Geraden:
+class RichtungsvektorErsteGleichung2Geraden:
     def __init__(self, richtungsvektor_x, richtungsvektor_y, richtungsvektor_z):
         self.richtungsvektor_z = richtungsvektor_z
         self.richtungsvektor_y = richtungsvektor_y
         self.richtungsvektor_x = richtungsvektor_x
 
 
-class StuetzvektorZweiteGleichung_2_Geraden:
+class StuetzvektorZweiteGleichung2Geraden:
     def __init__(self, stuetzvektor_x_2, stuetzvektor_y_2, stuetzvektor_z_2):
         self.stuetzvektor_z_2 = stuetzvektor_z_2
         self.stuetzvektor_y_2 = stuetzvektor_y_2
         self.stuetzvektor_x_2 = stuetzvektor_x_2
 
 
-class RichtungsvektorZweiteGleichung_2_Geraden:
+class RichtungsvektorZweiteGleichung2Geraden:
     def __init__(self, richtungsvektor_x_2, richtungsvektor_y_2, richtungsvektor_z_2):
         self.richtungsvektor_z_2 = richtungsvektor_z_2
         self.richtungsvektor_y_2 = richtungsvektor_y_2
@@ -96,7 +96,7 @@ def abfrage_1_gleichung():
     richtungsvektor_z = int(input("Richtungsvektor Koordinate z\n> "))
 
     ortsvektor_1_gleichung = OrtsvektorErsteGleichung(ortsvektor_x, ortsvektor_y, ortsvektor_z)
-    stuetzvektor_1_gleichung = StuetzvektorErsteGleichung_2_Geraden(stuetzvektor_x, stuetzvektor_y, stuetzvektor_z)
+    stuetzvektor_1_gleichung = StuetzvektorErsteGleichung(stuetzvektor_x, stuetzvektor_y, stuetzvektor_z)
     richtungsvektor_1_gleichung = RichtungsvektorErsteGleichung(richtungsvektor_x, richtungsvektor_y, richtungsvektor_z)
 
     return ortsvektor_1_gleichung, stuetzvektor_1_gleichung, richtungsvektor_1_gleichung
@@ -106,9 +106,9 @@ def gleichung_aufstellen_punktprobe(ortsvektor_1_gleichung, stuetzvektor_1_gleic
     t_x = ortsvektor_1_gleichung.ortsvektor_x - richtungsvektor_1_gleichung.richtungsvektor_x / \
           stuetzvektor_1_gleichung.stuetzvektor_x
     t_y = ortsvektor_1_gleichung.ortsvektor_y - richtungsvektor_1_gleichung.richtungsvektor_y / \
-          stuetzvektor_1_gleichung.stuetzvektor_y
+        stuetzvektor_1_gleichung.stuetzvektor_y
     t_z = ortsvektor_1_gleichung.ortsvektor_z - richtungsvektor_1_gleichung.richtungsvektor_z / \
-          stuetzvektor_1_gleichung.stuetzvektor_z
+        stuetzvektor_1_gleichung.stuetzvektor_z
 
     t = T(t_x, t_y, t_z)
 
@@ -140,17 +140,26 @@ def abfrage_2_gleichungen():
     richtungsvektor_y_2 = int(input("2. Gleichung: Richtungsvektor Koordinate y\n> "))
     richtungsvektor_z_2 = int(input("2. Gleichung: Richtungsvektor Koordinate z\n> "))
 
-    stuetzvektor_1 = StuetzvektorErsteGleichung_2_Geraden(stuetzvektor_x, stuetzvektor_y, stuetzvektor_z)
-    richtungsvektor_1 = RichtungsvektorErsteGleichung_2_Geraden(richtungsvektor_x, richtungsvektor_y, richtungsvektor_z)
-    stuetzvektor_2 = StuetzvektorZweiteGleichung_2_Geraden(stuetzvektor_x_2, stuetzvektor_y_2, stuetzvektor_z_2)
-    richtungsvektor_2 = RichtungsvektorZweiteGleichung_2_Geraden(richtungsvektor_x_2, richtungsvektor_y_2,
-                                                                 richtungsvektor_z_2)
+    stuetzvektor_1 = StuetzvektorErsteGleichung2Geraden(stuetzvektor_x, stuetzvektor_y, stuetzvektor_z)
+    richtungsvektor_1 = RichtungsvektorErsteGleichung2Geraden(richtungsvektor_x, richtungsvektor_y, richtungsvektor_z)
+    stuetzvektor_2 = StuetzvektorZweiteGleichung2Geraden(stuetzvektor_x_2, stuetzvektor_y_2, stuetzvektor_z_2)
+    richtungsvektor_2 = RichtungsvektorZweiteGleichung2Geraden(richtungsvektor_x_2, richtungsvektor_y_2, richtungsvektor_z_2)
 
     return stuetzvektor_1, stuetzvektor_2, richtungsvektor_1, richtungsvektor_2
 
 
-def gleichung_aufstellen_schnittpunkt():
-    pass
+def gleichung_aufstellen_schnittpunkt(stuetzvektor_1, stuetzvektor_2, richtungsvektor_1, richtungsvektor_2):
+    n = stuetzvektor_1.stuetzvektor_y + richtungsvektor_1.richtungsvektor_y * stuetzvektor_2.stuetzvektor_x - \
+        richtungsvektor_1.richtungsvektor_y * stuetzvektor_1.stuetzvektor_x / richtungsvektor_2.richtungsvektor_x / \
+        (richtungsvektor_1.richtungsvektor_y / -1) / richtungsvektor_2.richtungsvektor_x / 2
+
+    u = stuetzvektor_2.stuetzvektor_x + richtungsvektor_2.richtungsvektor_x * n - stuetzvektor_1.stuetzvektor_x / \
+        richtungsvektor_2.richtungsvektor_x
+
+    n = int(n)
+    u = int(u)
+
+    return u, n
 
 
 def punktprobe():
